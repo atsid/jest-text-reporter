aaa59e45408772ab0a9f374cd10cbafc8c11d554
Brad Maupin <brad.maupin@atsid.com>
2018-03-02 15:10:20 -0800

    cleanup

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # Custom Jest Reporter (jest-reporter)
 Jest text output reporter
 
-[![Build Status](https://travis-ci.org/trembacz/jest-reporter.svg?branch=master)](https://travis-ci.org/trembacz/jest-reporter)
-[![Dependencies](https://david-dm.org/trembacz/jest-reporter/status.svg)](https://david-dm.org/trembacz/jest-reporter?view=list) 
-[![devDependencies](https://david-dm.org/trembacz/jest-reporter/dev-status.svg)](https://david-dm.org/trembacz/jest-reporter?type=dev&view=list)
-
 ## Installing
 
 ```npm install --save-dev jest-text-reporter```
@@ -26,7 +22,7 @@ In your ```package.json``` add new reporter:
     [
       "<rootDir>/node_modules/jest-text-reporter", {
         "outputdir": "test-output",
-        "filename": "test-file"
+        "filename": "test-file.txt"
       }
     ]
   ]
--- index.js
+++ index.js
@@ -1,44 +1,40 @@
 const fs = require('fs');
 
-console.log('START');
-
-class jestReporter {
-	constructor(globalConfig, options) {
-
-		this.config = globalConfig;
+class jestTextReporter {
+    constructor(globalConfig, options) {
         this.options = options;
-	}
+    }
 
-	onRunComplete(contexts, results) {
-        //var filename = 'test-coverage/tests.txt';
-        var dir = this.options.outputdir || '.';
-        var filename = dir + '/' + (this.options.filename || 'tests.txt');
-        var text = "";
+    onRunComplete(contexts, results) {
+        const dir = this.options.outputdir || '.';
+        const filename = `${dir  }/${  this.options.filename || 'tests.txt'}`;
+        let text = '';
 
-        text += "Total tests: " + results.numTotalTests + "\r\n";
-        text += "Passed tests: " + results.numPassedTests + "\r\n";
-        text += "Failed tests: " + results.numFailedTests + "\r\n\r\n";
+        text += `Total test suites: ${  results.numTotalTestSuites  }\r\n`;
+        text += `Passed test suites: ${  results.numPassedTestSuites  }\r\n`;
+        text += `Failed test suites: ${  results.numFailedTestSuites  }\r\n\r\n`;
 
-        results.testResults.forEach(function suiteIterator(suiteResult) {
-            text += 'Test Suite - ' + suiteResult.testFilePath + '\r\n';
-            suiteResult.testResults.forEach(function testIterator(testResult) {
+        text += `Total tests: ${  results.numTotalTests  }\r\n`;
+        text += `Passed tests: ${  results.numPassedTests  }\r\n`;
+        text += `Failed tests: ${  results.numFailedTests  }\r\n`;
+
+        results.testResults.forEach((suiteResult) => {
+            text += `\r\nTest Suite - ${  suiteResult.testFilePath  }\r\n`;
+            suiteResult.testResults.forEach((testResult) => {
                 if (testResult.status === 'passed') {
-                    text += "passed - " + testResult.ancestorTitles + ' ' + testResult.title + "\r\n";
+                    text += `passed - ${ testResult.ancestorTitles } ${ testResult.title }\r\n`;
                 } else if (testResult.status === 'failed') {
-                    text += "failed - " + testResult.ancestorTitles + ' ' + testResult.title  + "\r\n";
+                    text += `failed - ${ testResult.ancestorTitles } ${ testResult.title }\r\n`;
                 } else if (testResult.status === 'pending') {
-
                 }
             });
         });
-        console.log('FIOLE');
-        if (!fs.existsSync(dir)){
+        if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir);
         }
         fs.writeFileSync(filename, text, 'utf-8');
-        console.log('FIOLE DPME');
         return results;
     }
 }
 
-module.exports = jestReporter;
+module.exports = jestTextReporter;
